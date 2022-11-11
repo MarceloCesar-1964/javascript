fef6501ba05e6398ebd39d9b4976e641b9e6cc73
Marcelo de Freitas Cesar <117288355+MarceloFreitasCesar@users.noreply.github.com>
2022-11-10 21:09:50 -0300

    adicionado exercicio 14

---
+++ exemplos/variavel var.js
@@ -0,0 +1,16 @@
+var x = 0
+console.log(typeof z)
+function a() {
+    var y = 2
+    console.log(x, y)
+    function b() {
+        x = 3
+        y = 4
+        z = 5
+    }
+    b ()
+    console.log(x, y, z)
+}
+a()
+console.log(x, z)
+console.log(typeof y)
---
+++ exercícios/aula014/ambiente.js
@@ -0,0 +1,10 @@
+/*
+var c = 1
+while (c <= 10) {
+    console.log(c)
+    c += 1
+}
+*/
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
